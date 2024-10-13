ac0f4862c227eb0aac026a1d6735a5bf0d2eba13
dips2701 <dipuwanjari@gmail.com>
2024-10-13 19:52:16 +0530

    Descriptive message for your changes

--- dataconnect-generated/js/default-connector/index.cjs.js
+++
@@ -1,9 +0,0 @@
-const { getDataConnect, validateArgs } = require('firebase/data-connect');
-
-const connectorConfig = {
-  connector: 'default',
-  service: 'Geoprofile',
-  location: 'us-central1'
-};
-exports.connectorConfig = connectorConfig;
-
